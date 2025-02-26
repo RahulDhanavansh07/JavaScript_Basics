69e816fa1b50178398259a9f411c7f27b42c766a
RahulDhanavansh07 <161809110+RahulDhanavansh07@users.noreply.github.com>
2025-02-26 13:53:15 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3);
+// we are using nodejs, not browser so there is diff syntax of alert in nodejs
+
+let name = "Rahul";
+let age = 22;
+let isLoggedIn = false;
+
+//number,  range>=2 to power 53
+//bigint
+//string = " hdh "
+//boolean = true/false
+// null >= standalone value
+//undefined => value not assign
+//symbol => mostly used for unique
+
+//object
+
+console.log(typeof undefined);
+
+//the type of null is object but the type of undefined is undefined
