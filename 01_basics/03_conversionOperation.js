29250870e1e03e38aad9f72840b3b02de69877d5
RahulDhanavansh07 <161809110+RahulDhanavansh07@users.noreply.github.com>
2025-03-07 14:51:57 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+
+// let score = 33;
+// let score = "33"; "33">=33 
+//let score = "33zbc";  //"33zbc" >= NaN
+let score = null;  // null >= 0
+// let score = true; true>= 1 
+// let score = "rahul"; rahul>= NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = "rahul";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 >= true; 0>= false;
+//"" >=false;
+//"rahul" >= true;
+
+
+
+let number = "0";
+
+let numberIntoString = String(number);
+console.log(numberIntoString);
+console.log(typeof(numberIntoString));
+
